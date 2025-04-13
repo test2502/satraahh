4c28cfb5a5a164d37da422fcf58da0656ba0c6d1
aniket190705 <aniket190705@gmail.com>
2025-04-13 22:46:23 +0530

    vercel update

---
+++ public/assets/picData.js
@@ -0,0 +1,76 @@
+const picData = {
+    pic1: {
+        src: "/assets/picIdeas/img1.jpg",
+        chooser: "Hanii",
+    },
+    pic2: {
+        src: "/assets/picIdeas/img2.jpg",
+        chooser: "Hanii",
+    },
+    pic3: {
+        src: "/assets/picIdeas/img3.jpg",
+        chooser: "Hanii",
+    },
+    pic4: {
+        src: "/assets/picIdeas/img4.jpg",
+        chooser: "Hanii",
+    },
+    pic5: {
+        src: "/assets/picIdeas/img5.jpg",
+        chooser: "Hanii",
+    },
+    pic6: {
+        src: "/assets/picIdeas/img6.jpg",
+        chooser: "Hanii",
+    },
+    pic7: {
+        src: "/assets/picIdeas/img7.jpg",
+        chooser: "Hanii",
+    },
+    pic8: {
+        src: "/assets/picIdeas/img8.jpg",
+        chooser: "Hanii",
+    },
+    pic9: {
+        src: "/assets/picIdeas/img9.jpg",
+        chooser: "Hanii",
+    },
+    pic10: {
+        src: "/assets/picIdeas/img10.jpg",
+        chooser: "Hanii",
+    },
+    pic11: {
+        src: "/assets/picIdeas/img11.jpg",
+        chooser: "Hanii",
+    },
+    pic12: {
+        src: "/assets/picIdeas/img12.jpg",
+        chooser: "Hanii",
+    },
+    pic13: {
+        src: "/assets/picIdeas/img13.jpg",
+        chooser: "Hanii",
+    },
+    pic14: {
+        src: "/assets/picIdeas/img14.jpg",
+        chooser: "Hanii",
+    },
+    pic15: {
+        src: "/assets/picIdeas/img15.jpg",
+        chooser: "Hanii",
+    },
+    pic16: {
+        src: "/assets/picIdeas/img16.jpg",
+        chooser: "Hanii",
+    },
+    pic17: {
+        src: "/assets/picIdeas/img17.jpg",
+        chooser: "Hanii",
+    },
+    pic18: {
+        src: "/assets/picIdeas/img18.jpg",
+        chooser: "Hanii",
+    },
+};
+
+export default picData;
